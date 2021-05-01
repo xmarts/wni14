5828d4926e49e741cb7f863be3298ee61a98362c
Axel-BeOpenMx <shelonga@live.com>
2021-04-30 20:31:07 -0500

    [ADD] account_landed_cost_rename: validation.

--- account_landed_cost_rename/models/stock_picking.py
+++ account_landed_cost_rename/models/stock_picking.py
@@ -2,10 +2,14 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
 from odoo import fields, models, api, _
+from odoo.exceptions import ValidationError
+
+import re
+
+CUSTOM_NUMBERS_PATTERN = re.compile(r'[0-9]{2}  [0-9]{2}  [0-9]{4}  [0-9]{7}')
 
 
 class StockPicking(models.Model):
-
     _inherit = 'stock.picking'
 
     customs_number = fields.Char(
@@ -30,4 +34,26 @@ class StockPicking(models.Model):
         )
     ]
 
+    def _get_custom_numbers(self):
+        self.ensure_one()
+        if self.customs_number:
+            return [num.strip() for num in self.customs_number.split(',')]
+        else:
+            return []
 
+    @api.constrains('customs_number')
+    def _check_customs_number(self):
+        stock_picking = self.env['stock.picking']
+        for picking in self:
+            custom_numbers = picking._get_custom_numbers()
+            if any(not CUSTOM_NUMBERS_PATTERN.match(custom_number) for custom_number in custom_numbers):
+                stock_picking |= picking
+
+        if not stock_picking:
+            return
+
+        raise ValidationError(_(
+            "Custom numbers set on invoice lines are invalid and should have a pattern like: 15  48  3009  0001234:\n%(invalid_message)s",
+            invalid_message='\n'.join(
+                '%s (id=%s)' % (p.customs_number, p.id) for p in stock_picking),
+        ))
